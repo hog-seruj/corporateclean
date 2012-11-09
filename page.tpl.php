6bb4f37ce36bb58900545ee7ac009795eaac7dee
George I. Tsopouridis <gtsopour@gmail.com>
2012-11-09 17:34:27 +0200

    [7.x-2.0] Responsive layout for page template (page.tpl.php) - http://drupal.org/node/1836558

--- page.tpl.php
+++ page.tpl.php
@@ -38,17 +38,19 @@
 	<!-- #header-menu-inside -->
     <div id="header-menu-inside" class="container_12 clearfix">
     
-        <div id="navigation" class="clearfix">
-        <?php if ($page['navigation']) :?>
-        <?php print drupal_render($page['navigation']); ?>
-        <?php else :
-        if (module_exists('i18n_menu')) {
-        $main_menu_tree = i18n_menu_translated_tree(variable_get('menu_main_links_source', 'main-menu'));
-        } else {
-        $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu')); 
-        }
-        print drupal_render($main_menu_tree);
-        endif; ?>
+    	<div class="grid_12">
+            <div id="navigation" class="clearfix">
+            <?php if ($page['navigation']) :?>
+            <?php print drupal_render($page['navigation']); ?>
+            <?php else :
+            if (module_exists('i18n_menu')) {
+            $main_menu_tree = i18n_menu_translated_tree(variable_get('menu_main_links_source', 'main-menu'));
+            } else {
+            $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu')); 
+            }
+            print drupal_render($main_menu_tree);
+            endif; ?>
+            </div>
         </div>
         
     </div><!-- EOF: #header-menu-inside -->
